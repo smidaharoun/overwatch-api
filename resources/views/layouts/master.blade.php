79e7c0ea77f874c8a1bb6d402283350b48a2e88f
James McFadden <james@jamesmcfadden.co.uk>
2016-08-01 12:54:17 +0100

    Meta tags

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,10 @@
 <html>
     <head>
-        <title>Overwatch API</title>
+        <title>Overwatch API - A RESTful API for the Overwatch Game</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="The Overwatch API is a project aiming to make available as much data about Overwatch as possible">
+        <meta name="author" content="James McFadden">
+
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300|Roboto+Slab" rel="stylesheet">
         <link rel="stylesheet" href="/css/normalize.css">
         <link rel="stylesheet" href="/css/skeleton.css">
