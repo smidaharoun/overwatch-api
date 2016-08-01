3c19fb73ca75e30bda499fad804d17a9653d7a68
James McFadden <james@jamesmcfadden.co.uk>
2016-08-01 12:27:05 +0100

    Add device width meta

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>Overwatch API</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300|Roboto+Slab" rel="stylesheet">
         <link rel="stylesheet" href="/css/normalize.css">
         <link rel="stylesheet" href="/css/skeleton.css">
