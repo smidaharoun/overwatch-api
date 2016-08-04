79eb25131787e6ed0da8c7999a37b9d2b820210d
James McFadden <james@jamesmcfadden.co.uk>
2016-08-04 16:09:00 +0100

    Add url attribute to Event model for JSON response

--- app/Event.php
+++ app/Event.php
@@ -6,8 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    protected $appends = ['url'];
+
     public function rewards()
     {
         return $this->hasMany('App\Reward');
+    }
+
+    public function getUrlAttribute()
+    {
+        return $this->attributes['url'] = route('api.event.show', ['id' => $this->attributes['id']]);
     }
 }
