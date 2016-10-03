149d73f2073b9b529b84bf14c3e28668171611dc
James McFadden <james@jamesmcfadden.co.uk>
2016-10-03 10:17:49 +0100

    Applied fixes from StyleCI (#22)

--- app/Http/Middleware/BuildApiResponse.php
+++ app/Http/Middleware/BuildApiResponse.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Response;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class BuildApiResponse
